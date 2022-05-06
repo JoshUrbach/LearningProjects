e64b1849cad7d7f96374a0ac3894315c8e09e93f
Josh Urbach <98100405+JoshUrbach@users.noreply.github.com>
2022-05-06 15:20:07 -0400

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -22,3 +22,10 @@ bool isPrime(int num) {
   return true;
     
 }
+
+
+//A much simpler option would be 
+//bool isPrime(int num){
+//  for(int i = 2; i <= sqrt(num);i++) if(num % i == 0) return false;
+//  return num <= 1? false: true; 
+//}
