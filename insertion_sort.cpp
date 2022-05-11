26b208a03b81cfef20996ac224fc5eb594a6d785
Josh Urbach <98100405+JoshUrbach@users.noreply.github.com>
2022-05-10 23:05:40 -0400

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+
+//Use insertion sort to sort the elements in ascending order
+void insertion_sort(int a[], int n){
+
+    for(int i = 1; i <=n-1; i++){
+
+        int current = a[i];
+
+        int prev = i - 1;
+
+        while (prev >=0 and a[prev] > current){
+            a[prev + 1] = a[prev];
+            prev = prev - 1;
+        } 
+
+        a[prev+1] = current;
+
+
+    }
+}
+
+int main() {
+	// your code goes here
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(arr)/sizeof(int);
+    insertion_sort(arr,n);
+
+    for(auto x: arr){
+        cout << x << ",";
+    }
+
+	return 0;
+}
