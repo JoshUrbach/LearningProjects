0f36e1fc36c760eac1092afa9a3251d9b3d42fc8
Josh Urbach <98100405+JoshUrbach@users.noreply.github.com>
2022-06-23 21:40:09 -0400

    Create sortCartesian.cpp

---
+++ sortCartesian.cpp
@@ -0,0 +1,7 @@
+//Function to sort a vector of pairs starting with x and if x is equal then with y.
+
+vector<pair<int, int>> sortCartesian(vector<pair<int, int>> v)
+{
+    sort(v.begin(), v.end());
+    return v;
+}
